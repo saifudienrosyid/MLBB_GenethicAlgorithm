1d7e9063ccd9456a13867dfd487bc7653d276d3f
saifudienrosyid <saif.rosy16@gmail.com>
2023-11-24 20:36:21 +0700

    ok

--- GAMLBB.py
+++ GAMLBB.py
@@ -1,9 +1,26 @@
-# Init population
+import numpy as np
+
+#fitness Function
+def fitness_strg1():
+    
+
+    return
+
+#Init population
+
 
 #selection
+
 
 #num_generations
 
 #num_parents_mating
 
-#
+#parent_selection_type
+
+#keep_parents
+
+#mutation_type
+
+#mutation_percent_gens
+
