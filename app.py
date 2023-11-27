44f5f07efb5a9681b330fae86b922318571f2438
muftialroaz <azamalfarokhul@gmail.com>
2023-11-27 07:22:12 +0700

    file direct withoout path

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ st.set_page_config(
     layout="centered", 
     )
 
-file_path = 'dataset/Mlbb_Heroes.csv'
-df, hero_df, total_he, total_tf = preprocess_data(file_path)
+df, hero_df, total_he, total_tf = preprocess_data('dataset/Mlbb_Heroes.csv')
 hero_data = hero_df.to_dict(orient='records')
 
 st.title('Genetic Algorithm MLBB Team Composition')
